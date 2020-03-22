cf536d93a7227b4117beec846029af4eb22a3680
Annie T Bellefleur <anniebellefleur@gmail.com>
2020-03-22 18:35:51 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,7 +8,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
 updatePlaylist(playlist, 'Davido', 'I love You')
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.Slowdive 
+  delete playlist[artistName]
   
   return playlist
 }
